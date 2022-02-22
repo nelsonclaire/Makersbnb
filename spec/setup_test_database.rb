152c13dd6ba16973f21d815efa0b15bdeb2d07b2
Claire Nelson <claire.nelson@johnlewis.co.uk>
2022-02-22 11:32:15 +0000

    Add database cpnnection info

---
+++ spec/setup_test_database.rb
@@ -0,0 +1,10 @@
+require 'pg'
+
+p "Setting up test database..."
+
+def setup_test_database
+  connection = PG.connect(dbname: 'makersbnb_test')
+
+  # connection.exec("TRUNCATE TABLE users CASCADE;")
+  connection.exec("TRUNCATE spaces, users;")
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,11 @@ require 'simplecov-console'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'rubocop'
 require_relative '../app.rb'
+require_relative './setup_test_database'
+
+# require File.join(File.dirname(__FILE__), '..', 'app.rb')
 #require_relative './setup_test_database'
 
 ENV['ENVIRONMENT'] = 'test'
@@ -26,6 +30,9 @@ Capybara.app = Makersbnb
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.before(:each) do |config|
+    setup_test_database
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
