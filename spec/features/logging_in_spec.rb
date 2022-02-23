db0e3b6559a73076a5d207693c145ebaf7d54be5
meghanblyth <megblyth@gmail.com>
2022-02-23 16:17:02 +0000

    Added login message to dates page

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -7,4 +7,14 @@ feature 'a signed-up user can log in' do
     expect(page).to have_content 'Enter Password'
     expect(page).to have_button 'Login'
   end
+
+  scenario 'loggin in takes the user to the dates page' do 
+    visit('/login')
+    user = create_test_user 
+    fill_in('email', with: 'test@gmail.com')
+    fill_in('password', with: 'password123')
+    click_button('Login')
+    expect(current_path).to eq '/dates'
+    expect(page).to have_content "Welcome, #{user.name}! You are logged in."
+  end 
 end
